748bc5735013c972d9c4ed57c47652e4def543ba
Paul Karikari <paulkarikari1@gmail.com>
2019-11-15 03:39:34 +0100

    fix processor utilization error

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,5 +1,6 @@
 #include "processor.h"
 #include <chrono>
+#include <iostream>
 #include <thread>
 #include "linux_parser.h"
 #include "tuple"
@@ -13,23 +14,25 @@ std::vector<int> fromStringsToInts(const std::vector<std::string>& cpuValues) {
   }
   return cpuValues_;
 }
-std::tuple<int, int> sumCpuValues(const std::vector<std::string>& cpuValues) {
+std::tuple<float, float> sumCpuValues(
+    const std::vector<std::string>& cpuValues) {
   auto cpuValuesInts = fromStringsToInts(cpuValues);
-  int idle =
+  float idle =
       cpuValuesInts[CPUStates::kIdle_] + cpuValuesInts[CPUStates::kIOwait_];
-  int nonIdle =
+  float nonIdle =
       cpuValuesInts[CPUStates::kUser_] + cpuValuesInts[CPUStates::kNice_] +
       cpuValuesInts[CPUStates::kSteal_] + cpuValuesInts[CPUStates::kIRQ_] +
       cpuValuesInts[CPUStates::kSoftIRQ_] + cpuValuesInts[CPUStates::kSteal_] +
       cpuValuesInts[CPUStates::kGuest_] + cpuValuesInts[CPUStates::kGuestNice_];
-  int total = idle + nonIdle;
+  float total = idle + nonIdle;
   return {total, idle};
 }
 
 // TODO: Return the aggregate CPU utilization
 float Processor::Utilization() {
   auto [total, idle] = sumCpuValues(LinuxParser::CpuUtilization());
-  std::this_thread::sleep_for(std::chrono::microseconds(1000));
+  std::this_thread::sleep_for(std::chrono::microseconds(100000));
   auto [total2, idle2] = sumCpuValues(LinuxParser::CpuUtilization());
-  return ((total2 - total) - (idle2 - idle)) / (total2 - total);
+
+  return 100.0 * (((total2 - total) - (idle2 - idle)) / (total2 - total));
 }
